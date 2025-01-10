b56526ce5c132042f92fdeb92271479bc08c60c1
Lavisha Saorha <goyalanurag10@gmail.com>
2025-01-10 09:38:09 +0530

    UPDATE frontend/ backend

--- public/src/utils/APIRoutes.js
+++
@@ -1,4 +0,0 @@
-const host = "http://localhost:5500";
-export const registerRoute = `${host}/api/auth/register`;
-export const loginRoute = `${host}/api/auth/login`;
-export const setAvatarRoute = `${host}/api/auth/setavatar`;
--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const {register, login, setAvatar} = require("../controllers/userControllers")
-const router= require("express").Router();
-
-router.post("/register",register);
-router.post("/login",login);
-router.post("/setAvatar/:_id",setAvatar);
-
-
-module.exports = router;
